837cfff93d047d093dbd2ccaf62f791cbb5c8c9d
yzyhk904 <86816519+yzyhk904@users.noreply.github.com>
2022-02-26 21:00:48 +0900

    Delete post-fs-data.sh
    No use.

--- post-fs-data.sh
+++
@@ -1,7 +0,0 @@
-#!/system/bin/sh
-# Do NOT assume where your module will be located. ALWAYS use $MODDIR if you need to know where this script and module is placed.
-# This will make sure your module will still work if Magisk change its mount point in the future
-# no longer assume "$MAGISKTMP=/sbin/.magisk" if Android 11 or later
-#
-# This script will be executed in post-fs-data mode
-#
